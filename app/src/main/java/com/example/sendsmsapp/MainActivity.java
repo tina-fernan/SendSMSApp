2903568debd596f8e52e1fe0b2c286c8d390784f
tina-fernan <tinaaroom@gmail.com>
2020-08-09 14:29:20 +0200

    update Methods

--- app/src/main/java/com/example/sendsmsapp/MainActivity.java
+++ app/src/main/java/com/example/sendsmsapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ActivityCompat.requestPermissions(MainActivity.this , new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+
+        editTextMessage = findViewById(R.id.editText);
+        editTextNumber = findViewById(R.id.editTextNumber);
+    }
+
+    public void sendSMS(View view){
+
+        String message = editTextMessage.getText().toString();
+        String number = editTextNumber.getText().toString();
+
+        SmsManager mySmsManager = SmsManager.getDefault();
+        mySmsManager.sendTextMessage(number, null, message, null, null);
+
     }
 }
