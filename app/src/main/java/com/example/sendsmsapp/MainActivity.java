fda0787e37ccb8af8feba598083298e9c9ecb8f6
tina-fernan <tinaaroom@gmail.com>
2020-08-09 13:49:10 +0200

    fist commit

---
+++ app/src/main/java/com/example/sendsmsapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sendsmsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNumber;
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this , new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+    }
+}
